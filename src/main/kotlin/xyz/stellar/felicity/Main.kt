20fe57ee1fc9b1ae03e118117632f4b42c07abb0
spectradevv <154019500+spectradevv@users.noreply.github.com>
2024-07-05 04:06:34 -0400

    Update Main.kt

--- src/main/kotlin/xyz/stellar/felicity/Main.kt
+++ src/main/kotlin/xyz/stellar/felicity/Main.kt
@@ -17,7 +17,7 @@ suspend fun main() {
         extensions {
             scannedExtensions.forEach {
                 add { it }
-                println("Loaded extensionn: ${it.name}")
+                println("Loaded extension: ${it.name}")
             }
         }
     }
